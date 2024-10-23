27a114e47d5c3059a7689da4e73c183f309b643c
Alex Culliere <nachasic@gmail.com>
2024-10-23 11:00:51 +0300

    Configure home-manager

--- configuration.nix
+++ configuration.nix
@@ -2,12 +2,13 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
     ];
 
   # Bootloader.
@@ -96,10 +97,15 @@
     #  thunderbird
     ];
   };
+  
+  # home-manager config
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users.alexc = import ./home.nix;
+  };
 
   # Install firefox.
   programs.firefox.enable = true;
-  #programs.hyprland.enable = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
--- flake.nix
+++ flake.nix
@@ -3,19 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    
+    hyprland = {
+    	url = "github:hyprwm/Hyprland";
+    	inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    # home-manager = {
-    #   url = "github:nix-community/home-manager";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
+      specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
-        # inputs.home-manager.nixosModules.default
+        inputs.home-manager.nixosModules.default
       ];
     };
   };
