6c3b8e51da88a63201eb939dade55930fe998b24
Alex Culliere <nachasic@gmail.com>
2025-01-16 11:11:02 +0200

    Add a screenshot overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -20,5 +20,17 @@
         ];
       });
     })
+
+    # Flameshot overlay
+    # https://github.com/NixOS/nixpkgs/issues/292700#issuecomment-2489523505
+    (final: prev: {
+      flameshot = prev.flameshot.overrideAttrs (previousAttrs: {
+        cmakeFlags = [
+          "-DUSE_WAYLAND_CLIPBOARD=1"
+          "-DUSE_WAYLAND_GRIM=1"
+        ];
+        buildInputs = previousAttrs.buildInputs ++ [ final.libsForQt5.kguiaddons ];
+      });
+    })
   ];
 }
